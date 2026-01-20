67e8daca5ad5db425cc4c9628095a95a7e40f92a
rudropratap-debug <rudropratapsharma640@gmail.com>
2026-01-20 22:45:58 +0600

    Activity selection

---
+++ Activity_selection/totalActivity_et.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter number of activity: ";
+    cin>>num;
+    
+    int st[num], et[num];
+
+    for(int i = 0; i < num; i++){
+        cout<<"Enter start time for activity "<<i+1<<": ";
+        cin>>st[i];
+
+        cout<<"Enter end time for activity "<<i+1<<": ";
+        cin>>et[i];
+    }
+
+    for(int i = 0; i < num - 1; i++){
+        for(int j = 0; j < num - i - 1; j++){
+            if(et[j] > et[j+1]){
+                swap(et[j],et[j+1]);
+                swap(st[j],st[j+1]);
+            }
+        }
+    }
+
+
+
+    int counter = 1;
+    int lastEnd = et[0];
+
+    for(int i = 0; i<num; i++){
+        if(st[i] >= lastEnd){
+            counter++;
+            lastEnd = et[i];
+        }
+    }
+
+
+    cout<<"Max number of activities : "<<counter<<endl;
+
+    return 0;
+}
